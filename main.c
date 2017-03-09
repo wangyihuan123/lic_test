3de1f7055013e449e62e14992036eebd92a1b45c
Benjamin.Wang <wangyihuan123@hotmail.com>
2017-03-09 17:09:56 +1300

    tidy the code and read test file for test

--- main.c
+++ main.c
@@ -2,7 +2,11 @@
 #include <macTypes.h>
 #include <string.h>
 
-static bool checkAnagram(void);
+#ifndef ALPHABET_NUMBER
+# define ALPHABET_NUMBER  26
+#endif
+
+static bool checkAnagram(char *input);
 void reverse(char *str);
 
 void reverse(char *str)
@@ -27,67 +31,93 @@ void reverse(char *str)
   }
 }
 
-static bool checkAnagram(void)
+static bool checkAnagram(char* input)
 {
-  char  Apl[26];
-  char input[128] = "cbcabacbc";
-  char backwards[128]="";
-  char oddtimes=0;
-  int temp3,temp4= 0;
+  char alphabet[ALPHABET_NUMBER];
+//  char input[128] = "aaaaabcdefgfedcbaaaaa";
+//  char backwards[128]="";
   int oddCounter = 0;
+  register size_t str_len = strlen(input);
 
-  for(int i=0;i<26;i++)
-  {
-    Apl[i] = '\0';
-  }
-  temp3 = StrLength(input);
-  temp4 = strlen(input);
-  for( int i = 0; i < strlen(input); i++ )
+  memset(alphabet, 0, sizeof(alphabet));
+
+  for( int i = 0; i < str_len; i++ )
   {
     char ch = input[i];
-    Apl[ch-'a']++;
+    alphabet[ch-'a']++;
   }
-  for( int cnt=0; cnt< 26;cnt++ )
+
+  for( int cnt=0; cnt< ALPHABET_NUMBER; cnt++ )
   {
-    //printf(oddOccur);
-    char tmp = Apl[cnt];
-    char tmp2;
-    if (Apl[cnt]%2 == 1) {
+    if (alphabet[cnt]&0x01)
       oddCounter++;
-      tmp2 = oddCounter % 2;
-      //if (oddOccur % 2 == 1) // more than 1 char should have odd frequency
-      //  oddtimes++;
-    }
-    //if( cnt%2 == 1 )
-    //  oddOccur++;
   }
-  if(oddCounter >1)
+
+  if(oddCounter > 1)
   {
-    printf("neither\n");
     return false;
   }
   else{
-    strcpy(backwards, input);
-    reverse(backwards);
-    if (strcmp(input, backwards) == 0)
-    {
-      printf("%s is a palindrone\n", input);
-    }
-    else{
-      printf("%s is a anogram\n", input);
-    }
 
     return true;
   }
-  //return true;
 }
 
+static bool
+checkPalindrone(char *input) {
 
+  int size = sizeof(input);
+  char backwards[size-1];
 
+//  memset(backwards,0, sizeof(backwards));
+  strcpy(backwards, input);
+  reverse(backwards);
+  if (strcmp(input, backwards) == 0)
+  {
+    return true;
+  }
+  else{
+    return false;
+  }
+
+};
+
+#define  MAX_READ_SIZE  65535
 int main() {
+  FILE* fp;
+  char  line[MAX_READ_SIZE];
   bool result;
-
-  result = checkAnagram();
+//  char  *input;
+  fp = fopen("/Users/zxl2013/ClionProjects/lic_test/lic_test.data", "r");
+  if (!fp)
+  {
+    printf("open file error\n");
+    return 1;
+  }
+  while (fgets(line, sizeof(line), fp) != NULL)
+  {
+    size_t linelen = strlen(line) + 1;
+//    trim(line);
+    char input[linelen];
+    strcpy(input, line);
+//    line[0] = tolower(line[0]);
+    result = checkAnagram(input);
+    if (! result)
+    {
+      printf("%s is neither.\n", input);
+//      return 0;
+    }
+    else {
+      result = checkPalindrone(input);
+      if (result) {
+        printf("%s is a palindrone\n", input);
+//      return 0;
+      } else {
+        printf("%s is an anagram of a palindrome\n", input);
+//      return 0;
+      }
+    }
+  }
 
   return 0;
 }
